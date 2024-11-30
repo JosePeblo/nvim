21c417461b1835f35bc906664f6385e3ada370f1
Peblo <a01275676@tec.mx>
2024-11-30 01:54:19 -0600

    feat: terminal remaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,4 +3,5 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope projec
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git file search' })
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input('grep: ') });
+
 end)
--- lua/peblo/remap.lua
+++ lua/peblo/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<C-s>', vim.cmd.w) -- shame
+
+-- Exit terminal mode with esc
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { silent = true })
+
+vim.keymap.set('n', '<leader>t', ':belowright split | resize 8<CR>:terminal<CR>', { silent = true })
 
